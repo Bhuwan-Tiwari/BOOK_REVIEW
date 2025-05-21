a67c8ccb123f3349c1aa01a06e80a6c81838f51e
Bhuwan Tiwari <bhuwan1200@gmail.com>
2025-05-21 13:57:54 +0530

    Added body parser

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+
 require('dotenv').config();
 
 const connectDB = require('./config/db');
@@ -14,6 +16,9 @@ connectDB();
 
 
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(express.json());
 
 app.use('/api', userRoutes);
